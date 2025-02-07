baf7cf94ee0c4988f03c732a3b83eebec67a619b
Kenneth-Tong <ktong22@bu.edu>
2025-02-07 18:35:15 -0500

    Added cenetering of photos in about section

--- hw/urls.py
+++ hw/urls.py
@@ -7,4 +7,5 @@ from hw import views
 urlpatterns = [ 
     # path(r'', views.home, name="home"),
     path(r'', views.home_page, name="home"),
+    path(r'about', views.about, name="about"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- quotes/urls.py
+++ quotes/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [ 
     # path(r'', views.home, name="home"),
     path(r'', views.home_page, name="home"),
-    path(r'about', views.about, name="about"),
-    path(r'quote', views.quote, name="quote"),
-    path(r'showall', views.showAll, name="showall"),
+    path(r'about', views.about, name="about"), #go to the about section
+    path(r'quote', views.quote, name="quote"), #generate new quotes
+    path(r'showall', views.showAll, name="showall"), #show all the quotes and pictures
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
